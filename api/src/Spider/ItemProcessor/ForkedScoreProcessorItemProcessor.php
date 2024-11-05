375bddf5d9348816fa35e7eb3d4ad00411275a44
zero <teclaizerai@gmail.com>
2024-11-05 16:03:25 +0100

    Init. framework-bundle spider.

---
+++ api/src/Spider/ItemProcessor/ForkedScoreProcessorItemProcessor.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App\Spider\ItemProcessor;
+
+use RoachPHP\ItemPipeline\ItemInterface;
+use RoachPHP\ItemPipeline\Processors\ItemProcessorInterface;
+use RoachPHP\Support\Configurable;
+
+final class ForkedScoreProcessorItemProcessor implements ItemProcessorInterface
+{
+    use Configurable;
+
+    public function processItem(ItemInterface $item): ItemInterface
+    {
+        $minForkScore = (int) $this->option('min_fork_score');
+        if ($minForkScore >= (int) $item->get('fork', 0)) {
+            return $item->drop(
+                \sprintf('Fewer than %s fork scored', $this->option('min_fork_score'))
+            );
+        }
+
+        return $item;
+    }
+
+    private function defaultOptions(): array
+    {
+        return [
+            'min_fork_score' => 1,
+        ];
+    }
+}
