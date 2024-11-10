3446312cb29fec5732aacdbf3307efbacb748e83
zero <teclaizerai@gmail.com>
2024-11-10 07:01:59 +0100

    Update spider & notification service.

--- api/context/ingesting/src/Infrastructure/MessagingConfiguration/NotificationService.php
+++ api/context/ingesting/src/Infrastructure/MessagingConfiguration/NotificationService.php
@@ -4,13 +4,26 @@ namespace Ingesting\Infrastructure\MessagingConfiguration;
 
 use Ecotone\Messaging\Attribute\Asynchronous;
 use Ecotone\Modelling\Attribute\EventHandler;
+use Ingesting\Core\Port\Driving\ForRegisteringIngestedItem;
 
 class NotificationService
 {
+    public function __construct(
+        private readonly ForRegisteringIngestedItem $ingestionRegistry
+    ) {
+    }
+
     #[Asynchronous("async_ingesting")]
     #[EventHandler(endpointId: "notifyAboutNewDependantRepository")]
     public function notifyAboutNewDependantRepository(DependantRepositoryDetected $event): void
     {
         echo $event->repository . "\n";
+    }
+
+    #[Asynchronous("async_ingesting")]
+    #[EventHandler(endpointId: "registerIngestedItem")]
+    public function registerIngestedItemOn(DependantRepositoryDetected $event): void
+    {
+        $this->ingestionRegistry->registerIngestedItem($event->dependantFrom, $event->repository, $event->stared, $event->fork);
     }
 }
