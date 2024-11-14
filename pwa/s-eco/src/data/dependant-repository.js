69cfc769df009655844318da9c0c0006ddf32cf0
zero <teclaizerai@gmail.com>
2024-11-14 15:24:45 +0100

    Init pwa.

---
+++ pwa/s-eco/src/data/dependant-repository.js
@@ -0,0 +1,24 @@
+
+const parts = {
+  1: [
+    {
+      id: 1,
+      description:
+        'A robot head with an unusually large eye and teloscpic neck -- excellent for exploring high spaces.',
+      title: 'Large Cyclops',
+      type: 'heads',
+      cost: 1225.5,
+    },
+  ],
+  2: [
+    {
+      id: 2,
+      description: 'A friendly robot head with two eyes and a smile -- great for domestic use.',
+      title: 'Friendly Bot',
+      cost: 945.0,
+      type: 'heads',
+      onSale: true,
+    },
+  ]
+};
+export default parts;
---
+++ pwa/s-eco/src/views/IngestingView.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import DependantRepositoryData from '../data/dependant-repository.json'
+
+
+</script>
+
+<template>
+  <div class="ingesting">
+    <h1>This is an ingesting page</h1>
+
+    <!-- {{ DependantRepositoryData }} -->
+
+    <!-- <table style="{border:1}"> -->
+    <table style=" background-repeat:no-repeat; width:450px;margin:0;" cellpadding="0" cellspacing="0" border="1">
+      <tr v-for="item in DependantRepositoryData" :key="item.id">
+        <td>{{ item.id }} </td><td>{{ item.repository }}</td><td>{{ item.starred }} </td><td>{{ item.fork }}</td>
+      </tr>
+    </table>
+
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .ingesting {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
