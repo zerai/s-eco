cce98141e30da3563979f4eba1159cd12f64047a
zero <teclaizerai@gmail.com>
2024-11-18 19:37:18 +0100

    Update vite config (fake apis).

--- pwa/s-eco/vite.config.js
+++ pwa/s-eco/vite.config.js
@@ -15,4 +15,64 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+      //'/foo': 'http://localhost:4567',
+      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+      //'/api': {
+      //  target: 'http://jsonplaceholder.typicode.com',
+      //  changeOrigin: true,
+      //  rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+      // with RegExp: http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
+      //'^/fallback/.*': {
+      //  target: 'http://jsonplaceholder.typicode.com',
+      //  changeOrigin: true,
+      //  rewrite: (path) => path.replace(/^\/fallback/, ''),
+      //},
+      // Using the proxy instance
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          // proxy will be an instance of 'http-proxy'
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      },
+      '/api/ingesting/items': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          // proxy will be an instance of 'http-proxy'
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      },
+      // Proxying websockets or socket.io: ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
+      // Exercise caution using `rewriteWsOrigin` as it can leave the proxying open to CSRF attacks.
+      // '/socket.io': {
+      //  target: 'ws://localhost:5174',
+      //  ws: true,
+      //  rewriteWsOrigin: true,
+      // },
+    },
+  },
 })
