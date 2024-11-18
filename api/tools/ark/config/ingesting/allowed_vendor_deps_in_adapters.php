6a11008b65f8d46c9f66f962ce8b64b8950e28e9
zero <teclaizerai@gmail.com>
2024-11-18 23:43:21 +0100

    setup phparkitect (#10)
    * Tool: added phparkitect + config.
    
    * CI: added phparkitect job (commit-stage).
    
    * Chore: update composer scripts (phparkitect).

---
+++ api/tools/ark/config/ingesting/allowed_vendor_deps_in_adapters.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types=1);
+
+return [
+    'Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository',
+    'Doctrine\Persistence\ManagerRegistry',
+];
---
+++ api/tools/ark/config/ingesting/allowed_vendor_deps_in_core.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types=1);
+
+return [
+    'Doctrine\ORM\Mapping\Entity',
+    'Doctrine\ORM\Mapping\Table',
+];
