afecb05a6ac07ffb512562a95e2c6404b98c41a8
zero <teclaizerai@gmail.com>
2024-11-29 09:26:06 +0100

    Chore: composer added zenstruck/foundry.

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
